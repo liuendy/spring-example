c145c55f642658a1dcdaa9116cc771b1e0ed9c9d
OHTAKE Manabu <manabu2783@hotmail.com>
2013-09-19 07:58:17 +0900

    コンフィグを一カ所にまとめました。

--- boot-jetty/src/main/java/com/otkmnb/boot/example/config/BootShowcaseContextConfig.java
+++ boot-jetty/src/main/java/com/otkmnb/boot/example/config/BootShowcaseContextConfig.java
@@ -5,18 +5,22 @@ import java.util.concurrent.TimeUnit;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
+import javax.ws.rs.ApplicationPath;
 
+import org.glassfish.jersey.filter.LoggingFilter;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
+import org.glassfish.jersey.server.spring.SpringLifecycleListener;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.ServletContextInitializer;
 import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource("classpath:Context.xml")
-//@ComponentScan
+@ComponentScan("com.otkmnb.boot.example")
 public class BootShowcaseContextConfig {
     
     private int port = 1234;
@@ -36,10 +40,24 @@ public class BootShowcaseContextConfig {
 
         @Override
         public void onStartup(ServletContext container) throws ServletException {
-            container.setInitParameter("contextConfigLocation", "classpath:Context.xml");
             ServletRegistration.Dynamic dispatcher = container.addServlet("jersey", ServletContainer.class);
             dispatcher.setInitParameter("javax.ws.rs.Application", BootShowcaseJerseyConfig.class.getName());
             dispatcher.addMapping("/v1.0/*");
+        }
+
+    }
+    
+    @ApplicationPath("/v1.0")
+    public class BootShowcaseJerseyConfig extends ResourceConfig {
+        
+        public BootShowcaseJerseyConfig() {
+            packages("com.otkmnb.boot.example.resource");
+            packages(SpringLifecycleListener.class.getPackage().getName());
+            property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
+            property(ServerProperties.JSON_PROCESSING_FEATURE_DISABLE, true);
+            property(ServerProperties.MOXY_JSON_FEATURE_DISABLE, true);
+            property(ServerProperties.WADL_FEATURE_DISABLE, true);
+            register(LoggingFilter.class);
         }
 
     }
